48b2aaea5837b4636e4b6bd81bd1625d04c06d9f
DrSkull24 <nonofr80@hotmail.fr>
2025-12-29 23:56:23 +0100

    puts grid to the right and adds manual save

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,18 @@ const int FPS = 60;
 const int FRAME_DELAY = 1000 / FPS; 
 const int NB_ROWS = 10;
 const int NB_COLS = 10;
-bool dragging = false;
-Tile drag_value;
-bool dragged_grid[NB_ROWS][NB_COLS] = { false };
 
 Level level(NB_ROWS, NB_COLS);
+
+void save() {
+    std::string error;
+    if (!Validator::validate(level, error)) {
+        std::cerr << "Validation échouée: " << error << std::endl;
+    } else {
+        Exporter::exportToJson(level, "level_01.json");
+        std::cout << "Niveau exporté avec succès\n";
+    }
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -54,26 +61,6 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
-    if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-        int w, h;
-        SDL_GetWindowSize(window, &w, &h);
-        int col = (event->button.x * NB_COLS) / w;
-        int row = (event->button.y * NB_ROWS) / h;
-        if (row >= 0 && row < NB_ROWS && col >= 0 && col < NB_COLS) {
-            drag_value = static_cast<Tile>(1 - static_cast<int>(level.grid.getTile(col, row)));
-        }
-        dragging = true;
-        return SDL_APP_CONTINUE;
-    }
-    if (event->type == SDL_EVENT_MOUSE_BUTTON_UP) {
-        for (int row = 0; row < NB_ROWS; row++) {
-            for (int col = 0; col < NB_COLS; col++) {
-                dragged_grid[row][col] = false;
-            }
-        }
-        dragging = false;
-        return SDL_APP_CONTINUE;
-    }
     ImGui_ImplSDL3_ProcessEvent(event);
     return SDL_APP_CONTINUE;
 }
@@ -89,65 +76,90 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     ImGui_ImplSDL3_NewFrame();
     ImGui::NewFrame();
 
-    /*
-    static bool my_tool_active = true;
-    if (my_tool_active) {
-        ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
-        if (ImGui::BeginMenuBar())
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0,0));
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0,0));
+
+    
+    ImGui::SetNextWindowPos(viewport->Pos);
+    ImGui::SetNextWindowSize(ImVec2(viewport->Size.x, 0));
+    ImGui::Begin("Menu Bar", (bool *) 0, ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBackground);
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
         {
-            if (ImGui::BeginMenu("File"))
-            {
-                if (ImGui::MenuItem("Open..", "Ctrl+O")) {  }
-                if (ImGui::MenuItem("Save", "Ctrl+S"))   {  }
-                if (ImGui::MenuItem("Close", "Ctrl+W"))  { my_tool_active = false; }
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenuBar();
+            if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+            if (ImGui::MenuItem("Save", "Ctrl+S"))   { save();}
+            ImGui::EndMenu();
         }
+        ImGui::EndMenuBar();
+    }
+    
+    if ((io.KeyCtrl) && ImGui::IsKeyPressed(ImGuiKey_S)) {
+        save();
+    }
 
-        static ImVec4 my_color = { 0.4f, 0.7f, 0.0f, 1.0f };
-        // Edit a color stored as 4 floats
-        ImGui::ColorEdit4("Color", &my_color.x);
+    ImGui::End();
 
-        // Generate samples and plot them
-        float samples[100];
-        for (int n = 0; n < 100; n++)
-            samples[n] = sinf(n * 0.2f + ImGui::GetTime() * 1.5f);
-        ImGui::PlotLines("Samples", samples, 100);
+    ImGuiStyle& style = ImGui::GetStyle();
+    float titlebar_height = style.FramePadding.y * 2 + ImGui::GetFontSize();
 
-        // Display contents in a scrolling region
-        ImGui::TextColored(ImVec4(1,1,0,1), "Important Stuff");
-        ImGui::BeginChild("Scrolling");
-        for (int n = 0; n < 50; n++)
-            ImGui::TextColored(my_color, "%04d: Some text", n);
-        ImGui::EndChild();
-        ImGui::End();
-    }*/
+    ImVec2 size(viewport->Size.x * 0.5, viewport->Size.y - titlebar_height);
+    ImVec2 pos(viewport->Pos.x + size.x, viewport->Pos.y + titlebar_height);
+    ImGui::SetNextWindowPos(pos);
+    ImGui::SetNextWindowSize(size);
+    ImGui::Begin("Level Editor Grid");
 
-    if (dragging) {
-        float mouse_x = ImGui::GetMousePos().x;
-        float mouse_y = ImGui::GetMousePos().y;
-        int col = (mouse_x * NB_COLS) / w;
-        int row = (mouse_y * NB_ROWS) / h;
+    ImVec2 cursor_pos = ImGui::GetCursorScreenPos();
+    ImVec2 grid_size = ImGui::GetContentRegionAvail();
+    float cell_width = grid_size.x / NB_COLS;
+    float cell_height = grid_size.y / NB_ROWS;
+
+    float mouse_x = ImGui::GetMousePos().x - cursor_pos.x;
+    float mouse_y = ImGui::GetMousePos().y - cursor_pos.y;
+    int col = (mouse_x * NB_COLS) / grid_size.x;
+    int row = (mouse_y * NB_ROWS) / grid_size.y;
+    bool isCellCorrect = (row >= 0 && row < NB_ROWS && col >= 0 && col < NB_COLS);
+    static Tile drag_value;
+    static bool dragging = false;
+
+    if (ImGui::IsMouseClicked(ImGuiMouseButton_Left) && isCellCorrect) {
+        if (row >= 0 && row < NB_ROWS && col >= 0 && col < NB_COLS) {
+            drag_value = static_cast<Tile>(1 - static_cast<int>(level.grid.getTile(col, row)));
+        }
+        dragging = true;
+    }
+
+    if (dragging && isCellCorrect) {
         level.grid.setTile(col, row, drag_value);
+    }
+
+    if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
+        dragging = false;
     }
 
     for (int row = 0; row < NB_ROWS; row++) {
         for (int col = 0; col < NB_COLS; col++) {
-            ImGui::SetNextWindowPos(ImVec2(col * (w / NB_COLS), row * (h / NB_ROWS)));
-            ImGui::SetNextWindowSize(ImVec2(w / NB_COLS, h / NB_ROWS));
             char window_name[32];
-            snprintf(window_name, sizeof(window_name), "Cell %d, %d", row, col);
-            ImGui::Begin(window_name, nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
+            snprintf(window_name, sizeof(window_name), "Cell %d, %d", row, col);    
+            ImGui::BeginChild(window_name, ImVec2(cell_width, cell_height), ImGuiChildFlags_Borders, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar);
+            
             if (level.grid.getTile(col, row) == Tile::Wall) {
-                ImVec2 pmin = ImGui::GetWindowPos();
-                ImVec2 win_size = ImGui::GetWindowSize();
+                ImVec2 pmin = ImGui::GetCursorScreenPos();
+                ImVec2 win_size = ImGui::GetContentRegionAvail();
                 ImVec2 pmax = ImVec2(pmin.x + win_size.x, pmin.y + win_size.y);
                 ImGui::GetWindowDrawList()->AddRectFilled(pmin, pmax, IM_COL32(255, 255, 255, 255));
             }
-            ImGui::End();
+            ImGui::EndChild();
+            ImGui::SameLine();
+
         }
+        ImGui::NewLine();
     }
+    ImGui::PopStyleVar();
+    ImGui::PopStyleVar();
+
+    ImGui::End();
 
     // Rendering
     ImGui::Render();
@@ -176,11 +188,4 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result)
     ImGui_ImplSDLRenderer3_Shutdown();
     ImGui_ImplSDL3_Shutdown();
     ImGui::DestroyContext();
-    std::string error;
-    if (!Validator::validate(level, error)) {
-        std::cerr << "Validation échouée: " << error << std::endl;
-    } else {
-        Exporter::exportToJson(level, "level_01.json");
-        std::cout << "Niveau exporté avec succès\n";
-    }
 }
