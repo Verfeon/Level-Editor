1dcc65d079e1563138fc5c913ffd6a442c96ec67
DrSkull24 <nonofr80@hotmail.fr>
2025-12-31 15:43:00 +0100

    adds import

---
+++ src/Importer.cpp
@@ -0,0 +1,38 @@
+#include "Importer.h"
+#include "json.hpp"
+#include "nfd.h"
+#include <fstream>
+
+using json = nlohmann::json;
+std::ifstream Importer::searchFile() {
+    nfdchar_t *outPath = NULL;
+    nfdresult_t result = NFD_OpenDialog( "json", nullptr, &outPath );
+        
+    std::ifstream file(outPath);
+    free(outPath);
+    return file;
+}
+
+Level Importer::importFromJson() {
+    std::ifstream file = searchFile();
+
+    json j = json::parse(file);
+    int width = j["grid"]["width"];
+    int height = j["grid"]["height"];
+    Level level(width, height);
+    const auto& tiles = j["grid"]["tiles"];
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            level.grid.setTile(x, y, static_cast<Tile>(tiles[y][x].get<int>()));
+        }
+    }
+    const auto& entities = j["entities"];
+    for (const auto& e : entities) {
+        Entity entity;
+        entity.type = e["type"].get<std::string>();
+        entity.x = e["x"].get<int>();
+        entity.y = e["y"].get<int>();
+        level.entities.push_back(entity);
+    }
+    return level;
+}
